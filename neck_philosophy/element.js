9806f5ceb1dd057be18a2c777cfd566ae6e130b0
Auleiy <Auleiy@foxmail.com>
2024-09-19 21:10:24 +0800

    样式更新和复制成功提示的概念

--- neck_philosophy/element.js
+++ neck_philosophy/element.js
@@ -2,14 +2,14 @@ function Segment(text, interpretation, comment, date) {
     document.writeln("<div class=\"content\" id=\"" + date + "\">");
     document.writeln("<div class=\"header-container\">")
     document.writeln("<span>" + date + "</span>");
-    document.writeln("<input class=\"copylink-btn btn\" type=\"button\" value=\"§\" title=\"复制跳转链接\" data-clipboard-text=\"" + window.location.href.slice(0, window.location.href.indexOf('#')) + '#' + date + "\"><br>");
+    document.writeln("<input class=\"copylink-btn btn\" type=\"button\" value=\"§\" title=\"复制跳转链接\" data-clipboard-text=\"" + window.location.href.slice(0, window.location.href.indexOf('#')) + '#' + date + "\" onclick=\"alert(\'复制成功！\')\"><br>");
     document.writeln("</div>")
     document.writeln("<span style=\"font-size: x-large;\">" + text + "</span><br>");
     document.writeln("<div class=\"content-description\">");
     document.writeln("<span>[理解] " + interpretation + "</span><br>");
     document.writeln("</div>");
     if (comment.length !== 0) {
-        document.writeln("<hr>");
+        document.writeln("<hr class=\"black\">");
         document.writeln("<div class=\"content-description\">");
         document.writeln("<span style=\"font-size: larger;\">评论</span>");
         document.writeln("</div>");
@@ -18,9 +18,16 @@ function Segment(text, interpretation, comment, date) {
             document.writeln("<div class=\"content-description\">");
             document.writeln("<span>" + v + "</span><br>");
             if (i !== comment.length - 1)
-                document.writeln("<hr>");
+                document.writeln("<hr class=\"half\">");
             document.writeln("</div>");
         }
     }
     document.writeln("</div>");
+}
+
+function alertElement() {
+    var a = [];
+    a.push(
+        document.createElement("")
+    )
 }
